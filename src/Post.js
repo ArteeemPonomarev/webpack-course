45fbaee17b1be7520692f97e4e7ab073d3cb3c7e
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-05-16 03:13:58 +0300

    first 50min of course

---
+++ src/Post.js
@@ -0,0 +1,19 @@
+class Post {
+    constructor(title) {
+        this.title = title
+        this.date = new Date()
+    }
+
+    toString() {
+        return JSON.stringify({
+            title: this.title,
+            date: this.date.toJSON()
+        })
+    }
+
+    get uppercaseTitle(){
+        return this.title.toUpperCase()
+    }
+}
+
+export default Post;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Post from './Post';
+
+const post = new Post ('Webpack Post Title')
+
+console.log(post.toString())
